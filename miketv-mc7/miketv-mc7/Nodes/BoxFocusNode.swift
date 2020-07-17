f7144e90698bbe9ac929c6d3560835e9aa1965d6
Juliana Vigato Pavan <ju.vigato3@gmail.com>
2020-07-17 13:58:05 -0300

    Adding new file to box puzzle

---
+++ miketv-mc7/miketv-mc7/Nodes/BoxFocusNode.swift
@@ -0,0 +1,20 @@
+//
+//  BoxFocusNode.swift
+//  miketv-mc7
+//
+//  Created by Juliana Vigato Pavan on 17/07/20.
+//  Copyright © 2020 gabriel. All rights reserved.
+//
+
+import SpriteKit
+
+class BoxFocusNode: SelectionableNode {
+    override func didTap() {
+        
+        guard
+            let model = delegate?.getModel()
+        else { return }
+        
+//        if model.scene1.
+    }
+}
