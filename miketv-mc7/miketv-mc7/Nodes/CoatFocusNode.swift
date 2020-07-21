5cbceba1479d4c88808256bf5e843790654b9006
Juliana Vigato Pavan <ju.vigato3@gmail.com>
2020-07-21 16:16:23 -0300

    Adding the change of the coat asset

--- miketv-mc7/miketv-mc7/Nodes/CoatFocusNode.swift
+++ miketv-mc7/miketv-mc7/Nodes/CoatFocusNode.swift
@@ -6,9 +6,40 @@
 //  Copyright © 2020 gabriel. All rights reserved.
 //
 
+import SpriteKit
+
 class CoatFocusNode: SelectionableNode {
     
     override func didTap() {
-        delegate?.changeScene(to: .CoatFocus)
+        guard
+            let model = delegate?.getModel()
+        else { return }
+        
+        let currentState = model.scene2.coatState
+        
+        let nextState = getNextState(current: currentState)
+        
+        if nextState != .destroyed,
+            let newTexture = model.scene2.coatTextures[nextState] {
+            model.scene2.coatState = nextState
+            self.texture = SKTexture(imageNamed: newTexture)
+        }
+        
+        if nextState == .openEmpty {
+            model.scene2.coatState = .destroyed
+        }
+    }
+    
+    private func getNextState(current: CoatState) -> CoatState {
+        switch current {
+        case .normal:
+            return .openWithMap
+        case .openWithMap:
+            return .openEmpty
+        case .openEmpty:
+            fallthrough
+        case .destroyed:
+            return .destroyed
+        }
     }
 }
